95f4a9239c26a35ba724dd0a480653fe054de8d6
roypriyasa <roypriyasa730@gmail.com>
2025-07-04 13:47:02 +0530

    Refactor App component to display student details in a table format

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,9 +2,39 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = "Presidency University";
+  const students = [
+    { name: "John Doe", RollNo: "101", course: "Computer Science", Age: 20, Email: "priyasaroy@gmail.com" },
+    { name: "Jane Smith", RollNo: "102", course: "Mathematics", Age: 22, Email: "tapu@gamil.com" },
+    { name: "Alice Johnson", RollNo: "103", course: "Physics", Age: 21, Email: "papan@gmail.com" }
+  ]
   return (
     <div className="App">
-      <h1>Hello, Priyasa!</h1>
+      <h1>Hello, {name}!</h1>
+      <h2>Student Details</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>RollNo</th>
+            <th>Course</th>
+            <th>Age</th>
+            <th>Email</th>
+
+          </tr>
+        </thead>
+        <tbody>
+          {students.map((std, index) => (
+            <tr key={index}>
+              <td>{std.name}</td>
+              <td>{std.RollNo}</td>
+              <td>{std.course}</td>
+              <td>{std.Age}</td>
+              <td>{std.Email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
