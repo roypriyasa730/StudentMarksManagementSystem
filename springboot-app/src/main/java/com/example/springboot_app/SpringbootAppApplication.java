8798136a90e5dd152800fac61b1fd8fdba52931a
roypriyasa <roypriyasa730@gmail.com>
2025-07-04 14:27:03 +0530

    Initialize Spring Boot application with Maven wrapper and basic project structure

--- springboot-app/src/main/java/com/example/springboot_app/SpringbootAppApplication.java
+++ springboot-app/src/main/java/com/example/springboot_app/SpringbootAppApplication.java
@@ -1,13 +1,13 @@
-package com.example.SpringBoot;
+package com.example.springboot_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class SpringbootAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(SpringbootAppApplication.class, args);
 	}
 
 }
--- springboot-app/src/test/java/com/example/springboot_app/SpringbootAppApplicationTests.java
+++ springboot-app/src/test/java/com/example/springboot_app/SpringbootAppApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.SpringBoot;
+package com.example.springboot_app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+class SpringbootAppApplicationTests {
 
 	@Test
 	void contextLoads() {
