f018a037de84ca7c616c1914fe0df20828d345b3
roypriyasa <roypriyasa730@gmail.com>
2025-07-12 21:16:33 +0530

    Swagger

--- springboot-app/src/main/java/com/RepositiryStudent.java
+++ springboot-app/src/main/java/com/RepositiryStudent.java
@@ -3,10 +3,10 @@ package com;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.springboot_app.Student;
+import com.example.springboot_app.StudentEntity;
 
 @Repository
-public interface RepositiryStudent extends JpaRepository<Student, Long> {
+public interface RepositiryStudent extends JpaRepository<StudentEntity, Long> {
     // This interface will automatically provide CRUD operations for the Student
     // entity
     // Additional custom query methods can be defined here if needed
--- springboot-app/src/main/java/com/example/springboot_app/StudentEntity.java
+++ springboot-app/src/main/java/com/example/springboot_app/StudentEntity.java
@@ -4,7 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
 @Entity
-public class Student {
+public class StudentEntity {
 
     @Id
     private Long id;
