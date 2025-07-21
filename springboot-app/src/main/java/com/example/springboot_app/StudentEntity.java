419d40e059c9c7dd3133474a94c704d169ba924f
roypriyasa <roypriyasa730@gmail.com>
2025-07-21 23:04:06 +0530

    Add @GeneratedValue annotation to StudentEntity id field

--- springboot-app/src/main/java/com/example/springboot_app/StudentEntity.java
+++ springboot-app/src/main/java/com/example/springboot_app/StudentEntity.java
@@ -1,12 +1,15 @@
 package com.example.springboot_app;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class StudentEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
